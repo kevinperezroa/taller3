e39bdad3baf1aa0d12b8a135f51681c215a29e5c
kevinperezroa <kevinperezroa@gmail.com>
2025-03-16 11:43:51 -0500

    actualizacion de controller.py

--- app/controller.py
+++ app/controller.py
@@ -9,23 +9,23 @@ def get_persona_by_id(persona_id):
 
 def create_persona(persona_data):
     persona = Persona(
-        nombre = persona_data['nombre']
-        direccion = persona_data['direccion']
-        telefono = persona_data['telefono']
-        email = persona_data['telefono']
+        nombre = persona_data['nombre'],
+        direccion = persona_data['direccion'],
+        telefono = persona_data['telefono'],
+        email = persona_data['email']
     )
     db.session.add(persona)
     db.session.commit()
 
 def update_persona(persona_id,persona_data):
     persona = Persona.query.get_or_404(persona_id)
-        persona.nombre = persona_data['nombre']
-        persona.direccion = persona_data['direccion']
-        persona.telefono = persona_data['telefono']
-        persona.email = persona_data['telefono']
+    persona.nombre = persona_data['nombre']
+    persona.direccion = persona_data['direccion']
+    persona.telefono = persona_data['telefono']
+    persona.email = persona_data['telefono']
     db.session.commit()
 
-def delete_persona(persona_id)
+def delete_persona(persona_id):
     persona = Persona.query.get_or_404(persona_id)
     db.session.delete(persona)
     db.session.commit()
