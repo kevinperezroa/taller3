384a14b45df9a26cfdaa92bbc1db54357333ef66
kevinperezroa <kevinperezroa@gmail.com>
2025-03-15 23:05:02 -0500

    creacion de models.py

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from app import db
+
+class Persona(db.Model):
+    __tablename__='persona'
+    id   = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    direccion = db.Column(db.String(50),nullable=False)
+    telefono = db.Column(db.String(20),nullable=False)
+    email = db.Column(db.String(50),nullable=False)
+
+    def __repr__(self):
+        return f'<Persona {self.persona}>'
